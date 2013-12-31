9935da9a30cd913eea0295e69ec374c65bf8cdd5
Marcus <marcus.beach@ascendumims.com>
2013-12-31 18:49:52 -0500

    Update variables.rb

--- variables.rb
+++ variables.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby
-#Design Date 12/31/2013
-#Design By www.rubyadministrator.com
+#Designed Date: 12/31/2013
+#Designed By: www.rubyadministrator.com
 #
 #Create a Variables
-var = "Hello world"
+var = "Hello World!!"
 #
 #Put the variables on screen.
 puts var
